0310be2dd507c1c836716a0c52af6d22fd458ab0
manoj_koneni <manojkoneni311@gmail.com>
2020-04-22 20:07:01 +0530

    Update BackPropagationAlgorithm.py

--- BackPropagationAlgorithm.py
+++ BackPropagationAlgorithm.py
@@ -1,1 +1,21 @@
 import numpy as np
+
+class BackPropagation:
+    def __init__(self, Weights, activation_function, inputs, target, learning_rate):
+        self.layers = len(Weights)
+        self.Weights = Weights
+        self.target = target
+        self.input = inputs
+        self.lr = learning_rate
+        self.act = activation_function
+    
+    def calculate_gradients(self):
+        L = self.layers
+        error = self.target-self.input[L]
+        grad = []
+        for i in range(L):      #for each layer
+            if i == 0:
+                local_grad = self.lr*error*
+            else:
+                local_gradients = np.zeros(self.Weights.shape[0])
+                
