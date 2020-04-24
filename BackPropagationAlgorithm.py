386580b30e46e9c9465932cbf4258654d65d8917
manoj_koneni <manojkoneni311@gmail.com>
2020-04-24 12:33:34 +0530

    Update BackPropagationAlgorithm.py
    add weight update step

--- BackPropagationAlgorithm.py
+++ BackPropagationAlgorithm.py
@@ -1,21 +1,33 @@
 import numpy as np
 
 class BackPropagation:
-    def __init__(self, Weights, activation_function, inputs, target, learning_rate):
-        self.layers = len(Weights)
-        self.Weights = Weights
+    def __init__(self, Weights, input, target, activation_function, learning_rate):
+        self.W = Weights
+        self.input = input
         self.target = target
-        self.input = inputs
-        self.lr = learning_rate
         self.act = activation_function
-    
-    def calculate_gradients(self):
-        L = self.layers
-        error = self.target-self.input[L]
+        self.lr = learning_rate
+
+    def calculate_grad(self):
+        output_error = (self.target-self.input[-1])[0]
+        local_grads = []
         grad = []
-        for i in range(L):      #for each layer
-            if i == 0:
-                local_grad = self.lr*error*
-            else:
-                local_gradients = np.zeros(self.Weights.shape[0])
-                
+        L = len(self.input)
+        for i in range(1,L):      #number of layers
+            grads = np.zeros(self.W[L-i-1].shape)
+            local = []
+            for j in range(self.W[L-i-1].shape[0]):
+                if i==1:                                        ## for last layer
+                    local_grad = -output_error[j]*self.input[-1][0][j]*(1-self.input[-1][0][j])
+                    local.append(local_grad)
+                else:                                           ## for all other hidden layers
+                    local_grad = self.input[L-i][0][j]*(1-self.input[L-i][0][j])
+                    p=0
+                    for h in range(self.input[L-i+1].shape[1]):
+                        p+=local_grads[-1][h]*self.W[L-i][h][j]
+                    local_grad*=p
+                    local.append(local_grad)
+                for k in range(self.W[L-i-1].shape[1]):
+                    grads[j][k] = self.lr*local_grad*self.input[L-i-1][0][k]
+            local_grads.append(local)
+            grad.append(grads)
