fa3d6bc7d042fd40650f42ec8e8731fabf434ef8
manoj_koneni <manojkoneni311@gmail.com>
2020-04-28 15:03:39 +0530

    mlp comments added

--- MLP/MLP.py
+++ MLP/MLP.py
@@ -1,15 +1,15 @@
 import numpy as np
 
-class MLP:
+class MLP:      ##multi layer perceptron
     def __init__(self, layer_dimensions, lr, activation_function):
         self.W = []
         for i in range(len(layer_dimensions)-1):
             Weights = np.zeros((layer_dimensions[i+1], layer_dimensions[i]))
             self.W.append(Weights)
         self.lr = lr
-        self.act = activation_function
+        self.act = activation_function      ##sigmoid is used for now
 
-    def FeedForward(self, input):
+    def FeedForward(self, input):           ##feed the input forward into the network and calculate layer wise outputs
         L = len(self.W)
         layer_wise_outputs = []
         layer_wise_outputs.append(input)
@@ -18,19 +18,22 @@ class MLP:
             layer_wise_outputs.append(output)
         return layer_wise_outputs
 
-    def calculate_grad(self, target, layer_wise_outputs):
-        output_error = (target-layer_wise_outputs[-1])[0]
+    def calculate_grad(self, target, layer_wise_outputs):   ##returns the gradients based on the target vector and output from FeedForward() 
+        ## gradient(j) = learning rate * local gradient(i) * input(j)
+        ## local gradient(i) = -(target(i)-output(i))*sigmoid'(i)     if i is the last layer
+        ## local gradient(i) = sigmoid'(i)*sum(W(k)*local gradient(k)) if i is the hidden layer and k is the next layer in forward direction
+        last_layer_error = (target-layer_wise_outputs[-1])[0]
         local_grads = []
-        grad = []
-        L = len(layer_wise_outputs)
-        for i in range(1,L):      #number of layers
-            grads = np.zeros(self.W[L-i-1].shape)
+        grad = []       
+        L = len(layer_wise_outputs)         ##number of layers
+        for i in range(1,L):
+            grads = np.zeros(self.W[L-i-1].shape)       ##layer wise gradients
             local = []
             for j in range(self.W[L-i-1].shape[0]):
-                if i==1:                                        ## for last layer
-                    local_grad = -output_error[j]*layer_wise_outputs[-1][0][j]*(1-layer_wise_outputs[-1][0][j])
+                if i==1:                     ## for the last layer
+                    local_grad = -last_layer_error[j]*layer_wise_outputs[-1][0][j]*(1-layer_wise_outputs[-1][0][j])
                     local.append(local_grad)
-                else:                                           ## for all other hidden layers
+                else:                        ## for all other hidden layers
                     local_grad = layer_wise_outputs[L-i][0][j]*(1-layer_wise_outputs[L-i][0][j])
                     p=0
                     for h in range(layer_wise_outputs[L-i+1].shape[1]):
@@ -43,7 +46,7 @@ class MLP:
             grad.append(grads)
         return grad
 
-    def backprop(self, grad):
+    def backprop(self, grad):               ##update the weights on the networks based on gradents from calculate-gradients
         L=len(self.W)
         for i in range(L):
             grads = grad[i]
