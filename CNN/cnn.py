43dfb31528c17667d34ee2198b9a6e1d85c1ff82
manoj_koneni <manojkoneni311@gmail.com>
2020-05-05 22:13:48 +0530

    debugged convolution layer

--- CNN/cnn.py
+++ CNN/cnn.py
@@ -15,32 +15,52 @@ class convolution_layer:
 
     def convolve(self, input, activation_function='relu'):
         convolution_output = []
-        steps = int(np.floor((input.shape[0]-self.kernel_shape[0])/self.s))
-        for i in range(self.n_kernels):
-            img_out = np.zeros((steps,steps))
-            j,k = 0,0
-            while j < input[0].shape[0]:
-                while k < input[0].shape[1]:
-                    img_out[j,k] = np.sum(np.multiply(kernel[i], input[k:k+self.kernel_shape[0], j:j+self.kernel_shape[0]]))
-                    if activation_function == 'relu':                   
-                        img_out[j,k] = np.max(img_out[j,k],0)
-                k+=self.s
-            j+=self.s
-            convolution_output.append(img_out)
+        if type(input)==list:
+            steps1 = int(np.floor((input[0].shape[0]-self.kernel_shape[0])/self.s))
+            steps2 = int(np.floor((input[0].shape[1]-self.kernel_shape[1])/self.s))
+            for h in range(len(input)):
+                for i in range(self.n_kernels):
+                    img_out = np.zeros((steps1,steps2))
+                    j = 0
+                    while j < steps2:
+                        k = 0
+                        while k < steps1:
+                            img_out[k,j] = np.sum(np.multiply(self.kernel[i], input[h][k:k+self.kernel_shape[0], j:j+self.kernel_shape[1]]))
+                            if activation_function == 'relu':                   
+                                img_out[k,j] = np.max(img_out[k,j],0)
+                            k+=self.s
+                        j+=self.s
+                    convolution_output.append(img_out)
+        else:
+            steps1 = int(np.floor((input.shape[0]-self.kernel_shape[0])/self.s))
+            steps2 = int(np.floor((input.shape[1]-self.kernel_shape[1])/self.s))
+            for i in range(self.n_kernels):
+                img_out = np.zeros((steps1,steps2))
+                j = 0
+                while j < steps2:
+                    k = 0
+                    while k < steps1:
+                        img_out[k,j] = np.sum(np.multiply(self.kernel[i], input[k:k+self.kernel_shape[0], j:j+self.kernel_shape[1]]))
+                        if activation_function == 'relu':                   
+                            img_out[k,j] = np.max(img_out[k,j],0)
+                        k+=self.s
+                    j+=self.s
+                convolution_output.append(img_out)
         return convolution_output
 
     def subsampling(self, input, pool_dim):          
-        steps = int(np.floor((input[0].shape[0]-pool_dim[0])/self.s))
+        steps1 = int(np.floor((input[0].shape[0]-pool_dim[0])/self.s))
+        steps2 = int(np.floor((input[0].shape[1]-pool_dim[1])/self.s))
         pool_output = []
         for i in range(len(input)):
-            img = np.zeros((steps,steps))
+            img = np.zeros((steps1,steps2))
             j,k = 0,0
-            while j < input[0].shape[0]:
-                while k < input[0].shape[1]:
+            while j < steps2:
+                while k < steps1:
                     if self.pooling_layer=='Max':
-                        img[j,k] = np.max(input[i][k:k+pool_dim[0], j:j+pool_dim[0]])
+                        img[k,j] = np.max(input[i][k:k+pool_dim[0], j:j+pool_dim[0]])
                     elif self.pooling_layer=='Average':
-                        img[j,k] = np.mean(input[i][k:k+pool_dim[0], j:j+pool_dim[0]])
+                        img[k,j] = np.mean(input[i][k:k+pool_dim[0], j:j+pool_dim[0]])
                     k+=self.s
                 j+=self.s
             pool_output.append(img)
