334db5679490fd14a4cf14e4007c9c7f72b5b13e
manoj_koneni <manojkoneni311@gmail.com>
2020-05-13 22:36:38 +0530

    debugged regression
    checked poly features for linear regression added lasso regression with coordinate descent

--- Regression_models/lasso_regression.py
+++ Regression_models/lasso_regression.py
@@ -12,5 +12,22 @@ class lasso_regression:
         self.W = np.random.randn(n,1)
         
         for i in range(iterations):
-            y_pred = np.dot(X, self.W)
-            error = Y-y_pred
+            for j in range(n):   #for every feature that exists
+                 
+                yi = np.dot(X,self.W)   #calculate the sum of weights multiplied by features
+                residuals = Y-yi      #calculating the error term
+                x_j = X[:,j].reshape(-1,1)     #taking only the jth feature values
+                
+                #cost = np.sum(residuals**2) + self.l1*np.sum(np.abs(self.w))   #computing cost
+                #self.cost_.append(cost)   
+                rho = np.dot(x_j.T,(residuals+self.w[j,:]*x_j))   #calculating rho, for the jth feature alone 
+                
+                if(rho<(-self.l1/2)):   #soft thresh holding
+                    self.w[j,:]=rho+(self.l1/2)
+                elif(rho>(self.l1/2)):
+                    self.w[j,:]=rho-(self.l1/2)
+                else:
+                    self.w[j,:]=0
+
+    def predict(self, X):
+        return np.dot(X, self.W)
--- Regression_models/regression.py
+++ Regression_models/regression.py
@@ -6,38 +6,44 @@ class regression:
         self.method = alg
         self.k = alg_params
         self.features = features
-        self.degree = 1
+        self.degree = degree
         self.W = []
 
-    def fit(self, X, Y):
+    def features_calci(self, X1):
+        m, n = X1.shape
         if self.features == 'poly':
             X2 = []
-            c = cr([j for j in range(X.shape[1])], self.degree)
+            c = cr([j for j in range(n)], self.degree)
+            print([j for j in range(n)])
             f = list(c)
-            for i in range(X.shape[0]):
-                w = np.ones((1,len(f)))
-                q = X[i][np.array(f)]
+            print(f)
+            for i in range(X1.shape[0]):
+                w = np.ones((1,len(f)))[0]
+                q = X1[i][np.array(f)]
                 for j in range(q.shape[1]):
                     w = w*q[:,j]
                 X2.append(w)
-            X = np.asarray(X2)
+            X1 = np.asarray(X2)
+        return X1
+        
+    def fit(self, X, Y):
+        m,n = X.shape
+        X1 = np.hstack((np.ones((m,1)), X))
+        n = X1.shape[1]
+        X1 = self.features_calci(X1)
+        #print(X1)
         if self.method == 'least_squares':
-            m,n = X.shape
-            X1 = np.hstack((np.ones(m,1), X))
             a = np.dot(X1.T, X1)
             b = np.linalg.inv(a)
             c = np.dot(b,X1.T)
             self.W = np.dot(c, Y)
         elif self.method == 'ridge':
-            m,n = X.shape
-            X1 = np.hstack((np.ones(m,1), X))
             a = np.dot(X1.T, X1)+ self.k*np.eye(X.shape[0])
             b = np.linalg.inv(a)
             c = np.dot(b,X1.T)
             self.W = np.dot(c, Y)
-        elif self.method == 'lasso':
-            
 
     def predict(self, X):
-        X1 = np.hstack((np.ones(X.shape[0],1),X))
+        X1 = np.hstack((np.ones((X.shape[0],1)),X))
+        X1 = self.features_calci(X1)
         return np.dot(X1, self.W)
