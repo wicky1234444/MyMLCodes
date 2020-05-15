120871c9e81eaaad568740e9e45a3939717ca2ac
manoj_koneni <manojkoneni311@gmail.com>
2020-05-15 13:53:25 +0530

    added AUROC

--- Metrics/evaluation_metrics.py
+++ Metrics/evaluation_metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy import trapz
 
 def confusion_matrix(true_labels, predictions):
     # output is dictionary containing classes, confusion matrix and measures like predictive values, recall
@@ -29,3 +30,16 @@ def F1_score(true_labels, predictions):
     for i in range(cm_dict['classes'].shape[0]):
         f1_score['class '+str(cm_dict['classes'][i])+' f1_score'] = (2*cm_dict['measures']['class '+str(cm_dict['classes'][i])+' precision']*cm_dict['measures']['class '+str(cm_dict['classes'][i])+' recall'])/(cm_dict['measures']['class '+str(cm_dict['classes'][i])+' precision']+cm_dict['measures']['class '+str(cm_dict['classes'][i])+' recall'])
     return f1_score
+
+def AUROC(true_labels, prediction_probabilities):
+    threshold = np.linspace(0,1, 20)
+    x1,y1 = [],[]
+    for t in threshold:
+        y = prediction_probabilities[:,1]
+        y = np.where(y<t, 0, y)
+        y = np.where(y>=t, 1, y)
+        d1 = confusion_matrix(true_labels, y)
+        y1.append(d1['measures']['class 1 recall'])
+        x1.append(1-d1['measures']['class -1 recall'])
+    area = trapz(y1, x= x1)
+    return area
