2092263f84467f0ee03a9cf01650cf5ec9ab13df
manoj_koneni <manojkoneni311@gmail.com>
2020-06-25 20:37:37 +0530

    added weighted entropy and gini calculation
    entropy is debugged, gini index needs debugging

--- Decision_Trees/tree_splitting_criterion.py
+++ Decision_Trees/tree_splitting_criterion.py
@@ -12,9 +12,10 @@ def Entropy(class1, class2=[], sample_weight=[]):
             return -np.sum(prob*np.log2(prob))
         else:
             entropy = 0
+            total_weights = np.sum(sample_weight)
             for j in range(len(unique_counts1[0])):
                 ind1 = np.where(class1==unique_counts1[0][j])[0]
-                p = np.sum(sample_weight[ind1])
+                p = np.sum(sample_weight[ind1])/total_weights
                 entropy+=(-p*np.log2(p))
             return entropy
     else:   ##groupby class2 and find entropy of each group
@@ -23,13 +24,18 @@ def Entropy(class1, class2=[], sample_weight=[]):
         prob = unique_counts[1]/size
         entropy=0
         classes = np.asarray(unique_counts[0])
+        if(len(sample_weight)!=0):
+            total_weights = np.sum(sample_weight)
         for i in range(len(classes)):  ##for each class
             ind = np.where(class2==classes[i])[0]
             if len(sample_weight)!=0:
-                p = Entropy(class1[ind], sample_weight[ind])
+                p = Entropy(class1[ind], sample_weight = sample_weight[ind])
+                #print(p, (np.sum(sample_weight[ind])/(total_weights)))
+                entropy+=(np.sum(sample_weight[ind])/(total_weights))*p
+                #print(entropy)
             else:
                 p = Entropy(class1[ind])
-            entropy+= prob[i]*p
+                entropy+= prob[i]*p
         return entropy
 
 def Information_Gain(class1, class2, sample_weight=[]):       ## change in entropy before and after split
@@ -40,6 +46,8 @@ def Gini_index(class1, class2, sample_weight=[]):
     unique_counts = np.unique(class2, return_counts=True)
     size = len(class2)
     prob = unique_counts[1]/size
+    if len(sample_weight)!=0:
+        total_weight = np.sum(sample_weight)
     gini=0
     classes = np.asarray(unique_counts[0])
     for i in range(len(classes)):  ##for each class
@@ -47,14 +55,17 @@ def Gini_index(class1, class2, sample_weight=[]):
         p1 = np.unique(class1[ind], return_counts=True)
         if len(sample_weight)==0:
             p2 = p1[1]/np.sum(p1[1])
-            p3 = np.sum(np.power(p2,2))
+            p3 = p2*(1-p2)
             gini+= p3*prob[i]
         else:
             p2 = 0
+            total_weight1 = np.sum(sample_weight[ind])
             for j in range(len(p1[0])):
                 ind1 = np.where(class1[ind]==p1[0][j])[0]
-                p2+=np.power(np.sum(sample_weight[ind1]),2)
-            gini+=p2*prob[i]
+                p3 = np.sum(sample_weight[ind1])/total_weight1
+                p3 = p3*(1-p3)
+                p2+= (total_weight1/total_weight1)*p3
+            gini+=p2
     return gini
 
 def Chi_square(class1, class2):
--- Ensemble_learning/Boosting/AdaBoost.py
+++ Ensemble_learning/Boosting/AdaBoost.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import sys
 sys.path.append('../../')
 from Decision_Trees.build_tree import *
@@ -12,21 +13,22 @@ class AdaBoost:
         self.estimators= {}
 
     def fit(self, X, Y):
-        self.w = np.zeros((X.shape[0]))
+        Y = Y.replace(0,-1)
+        self.w = np.asarray([1/(X.shape[0])]*X.shape[0])
         self.error = np.zeros((self.n_estimators))
-        for i in range(X.shape[0]):
-            self.w[i] = (1/X.shape[0])
         for i in range(self.n_estimators):
             if self.base_learner == 'dt':
-                dt = Decision_tree(max_depth=1, split_criterion='Entropy', node_eval='mode')        ## decision stumps
+                dt = Decision_tree(max_depth=2, split_criterion='Entropy', node_eval='mode')        ## decision stumps
                 dt.fit(X, Y, sample_weight=self.w)
                 y = dt.predict(X, dt.tree['root'])
                 e = 0
+                ind = Y.index
                 for j in range(X.shape[0]):
-                    if y[j]!=Y.loc[i]:
+                    if y[j]!=Y.loc[ind[i]]:
                         e+=self.w[i]
                 self.error[i] = 0.5*np.log((1-e)/e)
                 for j in range(X.shape[0]):
-                    if y[j]!=Y.loc[i]:
-                        self.w[i] = self.w[i]*np.exp(-self.error[i]*y[i]*Y.loc[i])
+                    if y[j]!=Y.loc[ind[i]]:
+                        self.w[i] = self.w[i]*np.exp(-self.error[i]*y[i]*Y.loc[ind[i]])
                 self.w/=np.sum(self.w)      ## renormalizing the weights
+        
